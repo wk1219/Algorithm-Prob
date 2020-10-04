1dce0d7bc24ea558be682c8369488f2e979d1e55
wk1219 <sjms1215@naver.com>
2020-10-04 15:11:57 +0900

    Update baekjoon-1260

--- Baekjoon/Step_prob/DFS-BFS/baekjoon-1260.py
+++ Baekjoon/Step_prob/DFS-BFS/baekjoon-1260.py
@@ -4,31 +4,31 @@ n, m, v = map(int, input().split())
 matrix = [[0] * (n+1) for i in range(n+1)]
 for i in range(m):
     x, y = list(map(int, input().split()))
-    matrix[x][y] = matrix[y][x] = 1
+    matrix[x][y] = matrix[y][x] = 1     # 대칭 표현
 
 visited = [False] * (n+1)
 
 
-def dfs(matrix, v, visited):
+def dfs(v):
     visited[v] = True
     print(v, end=' ')
     for i in range(1, n+1):
-        if visited[i] == False and matrix[v][i] == True:
-            dfs(matrix, i, visited)
+        if not visited[i] and matrix[v][i]:
+            dfs(i)
 
 
-def bfs(matrix, start, visited):
+def bfs(start):
     queue = deque([start])
     visited[start] = False
     while queue:
         v = queue.popleft()
         print(v, end=' ')
         for i in range(1, n+1):
-            if visited[i] == True and matrix[v][i] == True:
+            if visited[i] and matrix[v][i]:
                 queue.append(i)
                 visited[i] = False
 
 
-dfs(matrix, v, visited)
+dfs(v)
 print()
-bfs(matrix, v, visited)
+bfs(v)
