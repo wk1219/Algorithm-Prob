1db784ef9335bef02440a33dc221317341649a18
wk1219 <sjms1215@naver.com>
2020-10-07 21:43:15 +0900

    Solving prob baekjoon 10845

---
+++ Baekjoon/Step_prob/Queue_prob/baekjoon-10845.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+n = int(input())
+queue = deque()
+for i in range(n):
+    cmd = sys.stdin.readline().split()
+    if cmd[0] == 'push':
+        queue.append(int(cmd[1]))
+    elif cmd[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft())
+    elif cmd[0] == 'size':
+        print(len(queue))
+    elif cmd[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif cmd[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
