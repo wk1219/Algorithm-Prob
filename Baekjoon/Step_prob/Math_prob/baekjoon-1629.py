9f9102689a36e9e43c621a2083979b79aaa7e235
wk1219 <sjms1215@naver.com>
2020-10-13 23:14:58 +0900

    Solving prob baekjoon 1629

---
+++ Baekjoon/Step_prob/Math_prob/baekjoon-1629.py
@@ -0,0 +1,11 @@
+a, b, c = map(int, input().split())
+
+def power(a, b, m):
+    res = 1
+    while b > 0:
+        if b % 2 != 0:
+            res = (res * a) % m
+        b //= 2
+        a = (a * a) % m
+    return res
+print(power(a, b, c))
