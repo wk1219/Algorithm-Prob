1f6a2edd3546ed16a827d57a4223e5e892654511
wk1219 <sjms1215@naver.com>
2021-01-22 22:51:42 +0900

    Solving prob jump.py

---
+++ Programmers/BP/jump.py
@@ -0,0 +1,13 @@
+def solution(n):
+    # ans = 0
+    # while n != 0:
+    #     if n % 2 == 0:
+    #         n //= 2
+    #     else:
+    #         n -= 1
+    #         ans += 1
+    return bin(n).count('1')
+
+
+n = 5000
+print(solution(n))
